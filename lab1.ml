da8cf25edfb6fd8ae88cea4592b1a963cf46e311
Catherine Zeng <>
2018-02-01 16:28:56 -0500

    lab1

--- lab1.ml
+++ lab1.ml
@@ -42,7 +42,7 @@ exercise2 is a function that returns 42 (instead of failing). When you
 submit, the Exercise 2 unit test should then pass.
 ......................................................................*)
 
-let exercise2 () = failwith "exercise2 not implemented" ;;
+let exercise2 () = 42;;
 
 (* Note that your grade on labs is not dependent on your passing the
 unit tests. You'll see that even when a unit test is marked as passed,
@@ -99,7 +99,7 @@ appropriate OCaml expression to assign the value to the variable
 exercise1 below.
 ......................................................................*)
 
-let exercise3 () = failwith "exercise3 not implemented" ;;
+let exercise3 () = -(5 - 3);;
 
 (* Hint: The OCaml concrete expression
 
@@ -113,6 +113,16 @@ correspond to. Check it with a member of the course staff if you'd
 like.
 ......................................................................*)
 
+(*
+    -
+    ^
+   / \
+  /   \ 
+  -   3
+  |
+  5    
+
+*)
    
 (*======================================================================
 Part 2: Types and type inference
@@ -124,24 +134,22 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
 
-let exercise5a : ??? = 42 ;;
+let exercise5a : int = 42 ;;
 
-let exercise5b : ??? =
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : int*float -> int  =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : int -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : bool -> bool list =
   fun x -> if x then [x] else [] ;;
 
-remove this end of comment line too ----> *)
 
 (*======================================================================
 Part 3: First-order functional programming
@@ -151,9 +159,9 @@ like those from class before moving onto more complex problems. As a
 reminder, here's the definition for the length function of type int
 list -> int:
 
-let rec length (lst : int list) : int =
+let rec length (list : int list) : int =
   match lst with
-  | [] -> 0
+  [] -> 0
   | head :: tail -> 1 + length tail ;;
 
 ........................................................................
@@ -170,18 +178,22 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  [] -> []
+  | h :: t  -> h*h :: square_all t;;
 
-let exercise6 = [] ;;
+let exercise6 = square_all [1;2;3] ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
 list. (What's a sensible return value for the empty list?)
 ......................................................................*)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+  [] -> 0
+  | h :: t -> h + sum t;;
   
 (*......................................................................
 Exercise 8: Define a recursive function that returns the maximum
@@ -190,8 +202,11 @@ can raise an appropriate exception -- a Match_failure or
 Invalid_argument exception for instance.
 ......................................................................*)
 
-let max_list (lst : int list) : int =
-  failwith "max_list not implemented" ;;
+let rec max_list (lst : int list) : int =
+  match lst with
+  [] -> raise (Invalid_argument "empty list")
+  | [h] -> h 
+  | h :: t -> if h > max_list t then h else max_list t;;
 
 (*......................................................................
 Exercise 9: Define a function zip, that takes two int lists and
@@ -205,8 +220,14 @@ length lists, to just pad the shorter list with, say, false values, so
 that, zip [1] [2; 3; 4] = [(1, 2); (false, 3); (false, 4)]?
 ......................................................................*)
 
-let zip (x : int list) (y : int list) : (int * int) list =
-  failwith "zip not implemented" ;;
+let rec zip (x : int list) (y : int list) : (int * int) list =
+  if List.length x <> List.length y then raise (Invalid_argument "mismatched")
+  else
+    match x, y with
+      | [], [] -> []
+      | hx::tx, hy::ty -> (hx,hy) :: zip tx ty
+      | hx::tx, [] -> raise (Invalid_argument "mismatch")
+      | [], hx::tx -> raise (Invalid_argument "mismatch");;
 
 (*.....................................................................
 Exercise 10: Recall the definition of the function prods from lecture
